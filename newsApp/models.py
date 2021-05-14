ed1326bb4d57e370784b804f0bb078faceea5505
Aman Singhal <amansinghal.it1@gmail.com>
2021-05-15 01:21:10 +0530

    Get Data from Admin Panel for Master Page.

--- newsApp/models.py
+++ newsApp/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 class NewsAppModel(models.Model):
     name = models.TextField(max_length=255)
+    about = models.TextField(max_length=255, default='-')
+    fb = models.TextField(max_length=255, default='http://fb.com/')
+    twitter = models.TextField(max_length=255, default='http://twitter.com/')
+    youtube = models.TextField(max_length=255, default='http://youtube.com/')
 
     def __str__(self):
         return self.name
--- newsApp/views.py
+++ newsApp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+from .models import NewsAppModel
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'frontend/home.html')
+    # siteName = NewsAppModel.objects.filter(pk=2)
+    siteName = NewsAppModel.objects.get(pk=1)
+
+    return render(request, 'frontend/home.html', {'site': siteName})
 
 
 def about(request):
-    return render(request, 'frontend/about.html')
+    # siteName = NewsAppModel.objects.filter(pk=2)
+    siteName = NewsAppModel.objects.get(pk=1)
+
+    return render(request, 'frontend/about.html', {'site': siteName})
